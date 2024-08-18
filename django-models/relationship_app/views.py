95c912a1bb83df2cf7b38abf7c4c785ca10cd86c
medaminechihab97 <med.chihab97@gmail.com>
2024-08-18 22:52:50 +0100

    user login and logout

--- django-models/relationship_app/views.py
+++ django-models/relationship_app/views.py
@@ -4,7 +4,6 @@ from django.views.generic.detail import DetailView
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth import login, logout
 from django.contrib.auth.decorators import user_passes_test
-from rest_framework.permissions import BasePermission
 
 
 
@@ -50,7 +49,7 @@ def login_view(request):
 def logout_view(request):
   logout(request)
   return redirect('login')
-def Admin_view(request):
+def admin_view(request):
     # Only accessible to users with the 'Admin' role
     return render(request, 'admin_view.html')
 
@@ -74,15 +73,13 @@ def is_member(user):
 
 
 
-class IsAdminUser(BasePermission):
-    def has_permission(self, request, view):
-        return request.user.is_authenticated and request.user.userprofile.role == 'Admin'
 
 
 
 
 
-admin_view = user_passes_test(is_admin)(Admin_view)
+
+Admin = user_passes_test(is_admin)(admin_view)
 librarian_view = user_passes_test(is_librarian)(librarian_view)
 member_view = user_passes_test(is_member)(member_view) 
 
