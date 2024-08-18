436eed5fe1f609b1a42b91b80afb348f7e5379c8
medaminechihab97 <med.chihab97@gmail.com>
2024-08-18 22:41:08 +0100

    user login and logout

--- django-models/relationship_app/views.py
+++ django-models/relationship_app/views.py
@@ -48,7 +48,7 @@ def login_view(request):
 def logout_view(request):
   logout(request)
   return redirect('login')
-def admin_view(request):
+def Admin_view(request):
     # Only accessible to users with the 'Admin' role
     return render(request, 'admin_view.html')
 
@@ -69,6 +69,6 @@ def is_librarian(user):
 def is_member(user):
     return user.userprofile.role == 'Member'
 
-admin_view = user_passes_test(is_admin)(admin_view)
+Admin_view = user_passes_test(is_admin)(Admin_view)
 librarian_view = user_passes_test(is_librarian)(librarian_view)
 member_view = user_passes_test(is_member)(member_view)
