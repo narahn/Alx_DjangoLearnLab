ee44e624fd2bef27488ae8eb958d875a1f2a350c
medaminechihab97 <med.chihab97@gmail.com>
2024-08-18 21:11:29 +0100

    advanced django relationships

--- django-models/relationship_app/query_samples.py
+++ django-models/relationship_app/query_samples.py
@@ -1,11 +1,11 @@
 from relationship_app.models import Author, Book, Library, Librarian
 
-# Query all boks by a specific author
+# Query all books by a specific author
 def get_books_by_author(author_name):
   author = Author.objects.get(name=author_name)
   return Book.objects.filter(author=author)
 
-# List all books in a librery
+# List all books in a library
 def get_books_in_library(library_name):
   library = Library.objects.get(name=library_name)
   return library.books.all()
@@ -13,9 +13,9 @@ def get_books_in_library(library_name):
 # Retrieve the librarian for a library
 def get_librarian_for_library(library_name):
   library = Library.objects.get(name=library_name)
-  return library.librarian
+  return library.librarian  # Access librarian directly through ForeignKey
 
-# Example 
+# Example usage
 author_name = "John Doe"
 library_name = "Central Library"
 
