3bb11bee204dcf91870725be9ffbe223f9b3d44e
medaminechihab97 <med.chihab97@gmail.com>
2024-08-18 21:23:14 +0100

    advanced django relationships

--- django-models/relationship_app/urls.py
+++ django-models/relationship_app/urls.py
@@ -1,7 +1,7 @@
 from django.urls import path
-from . import views
+from .views import list_books  # Import the list_books function
 
 urlpatterns = [
-  path('books/', views.list_books, name='list_books'),
+  path('books/', list_books, name='list_books'),
   path('library/<int:pk>/', views.LibraryDetailView.as_view(), name='library_detail'),
 ]
