440226bf22f53c8c72092a7618302c52e81df0df
medaminechihab97 <med.chihab97@gmail.com>
2024-08-18 21:56:07 +0100

    user login and logout

--- django-models/relationship_app/urls.py
+++ django-models/relationship_app/urls.py
@@ -1,11 +1,13 @@
 from django.urls import path
 from .views import list_books  # Import the list_books function
 from . import views
+from django.contrib.auth.views import LoginView, LogoutView
+
 
 urlpatterns = [
   path('books/', list_books, name='list_books'),
   path('library/<int:pk>/', views.LibraryDetailView.as_view(), name='library_detail'),
-  path('login/', views.login_view, name='login'),
-  path('logout/', views.logout_view, name='logout'),
+  path('login/', LoginView.as_view(template_name='registration/login.html'), name='login'),
+  path('logout/', LogoutView.as_view(template_name='registration/logout.html'), name='logout'),
   path('register/', views.register_view, name='register'),
 ]
