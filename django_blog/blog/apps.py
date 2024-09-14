42d4cede6042b276497d08d5011389eda6db3f51
narahn <narahosni@gmail.com>
2024-09-14 02:05:24 +0100

    added anew file

--- django_blog/blog/apps.py
+++ django_blog/blog/apps.py
@@ -1,7 +1,6 @@
 from django.apps import AppConfig
 
-class BlogConfig(AppConfig):
-    name = 'blog'
 
-    def ready(self):
-        import blog.signals
+class BlogConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'blog'
--- django_blog/blog/models.py
+++ django_blog/blog/models.py
@@ -9,12 +9,3 @@ class Post(models.Model):
 
     def __str__(self):
         return self.title
-
-
-class Profile(models.Model):
-    user = models.OneToOneField(User, on_delete=models.CASCADE)
-    image = models.ImageField(default='default.jpg', upload_to='profile_pics')
-    bio = models.TextField(blank=True)
-
-    def __str__(self):
-        return f'{self.user.username} Profile'
