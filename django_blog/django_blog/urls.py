0dc053da23dcc21a5ae56b237a61d96ef9693f9b
narahn <narahosni@gmail.com>
2024-09-19 21:24:02 +0100

    added anew file

--- django_blog/django_blog/urls.py
+++ django_blog/django_blog/urls.py
@@ -2,7 +2,7 @@
 URL configuration for django_blog project.
 
 The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/4.2/topics/http/urls/
+    https://docs.djangoproject.com/en/5.1/topics/http/urls/
 Examples:
 Function views
     1. Add an import:  from my_app import views
@@ -15,32 +15,9 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
-from django.urls import path
-from django.contrib.auth import views as auth_views
-from . import views
-from django.urls import path
-from .views import PostListView, PostDetailView, PostCreateView, PostUpdateView, PostDeleteView
+from django.urls import path , include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('login/', auth_views.LoginView.as_view(template_name='blog/login.html'), name='login'),
-    path('logout/', auth_views.LogoutView.as_view(template_name='blog/logout.html'), name='logout'),
-    path('register/', views.register, name='register'),
-    path('profile/', views.profile, name='profile'),
-    path('', PostListView.as_view(), name='post-list'),  # List all posts
-    path('post/<int:pk>/', PostDetailView.as_view(), name='post-detail'),  # View details of a single post
-    path('post/new/', PostCreateView.as_view(), name='post-create'),  # Create a new post
-    path('post/<int:pk>/update/', PostUpdateView.as_view(), name='post-update'),  # Update an existing post
-    path('post/<int:pk>/delete/', PostDeleteView.as_view(), name='post-delete'),  # Delete a post
+    path('', include('blog.urls')), # This line adds the URL configuration for blog app to the main URL configuration.
 ]
-
-
-
-urlpatterns = [
-
-]
-
- 
-
- 
